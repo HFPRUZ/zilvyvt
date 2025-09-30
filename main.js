894e1d444861bb8c2a42179fed2461267ffa9ec8
HFPRUZ <heflaruz@gmail.com>
2025-09-30 15:46:56 +0200

    falta ICO y diseño de enlaces

--- main.js
+++ main.js
@@ -1,18 +1,32 @@
+// Limpiar sesión de prueba
 sessionStorage.removeItem('isAgeVerified');
+
+// GIFs disponibles
+const gifs = [
+    "images/zilvyvtdance.gif",
+    "images/zilvyvtfall.gif",
+    "images/zilvyvtflat.gif",
+    "images/zilvyvtstamped.gif",
+    "images/zilvyvtthoughtful.gif",
+    "images/zilvyvtwiggle.gif",
+    "images/zilvywalk.gif"
+];
+
+let leftIndex = 0;
+let rightIndex = 1;
+let mobileIndex = 0;
+
+// Verificación de edad
 document.addEventListener('DOMContentLoaded', () => {
     const ageConfirmed = sessionStorage.getItem('isAgeVerified');
-    
     if (!ageConfirmed) {
         document.getElementById('age-gate-modal').style.display = 'flex';
     } else {
         loadContent(ageConfirmed === 'true');
     }
+    changeGifs();
+    setInterval(changeGifs, 20000);
 });
-
-const IMAGE_PATHS = {
-    REGULAR: 'images/imagen-vtuber.jpg',
-    ADULT: 'images/imagen-vtuber-adult.jpg'
-};
 
 function verifyAge(isVerified) {
     document.getElementById('age-gate-modal').style.display = 'none';
@@ -22,15 +36,28 @@ function verifyAge(isVerified) {
 
 function loadContent(showAdultContent) {
     document.getElementById('main-content').style.display = 'block';
-    
     const profileImage = document.getElementById('profile-pic');
     const restrictedLink = document.getElementById('restricted-link');
-
     if (showAdultContent) {
-        profileImage.src = IMAGE_PATHS.ADULT;
+        profileImage.src = 'images/imagen-vtuber-adult.jpg';
         restrictedLink.style.display = 'block';
     } else {
-        profileImage.src = IMAGE_PATHS.REGULAR;
+        profileImage.src = 'images/imagen-vtuber.jpg';
         restrictedLink.style.display = 'none';
+    }
+}
+
+// Cambiar GIFs
+function changeGifs() {
+    if (window.innerWidth > 768) {
+        // PC
+        document.getElementById("left-gif").src = gifs[leftIndex];
+        document.getElementById("right-gif").src = gifs[rightIndex];
+        leftIndex = (leftIndex + 2) % gifs.length;
+        rightIndex = (rightIndex + 2) % gifs.length;
+    } else {
+        // Móvil
+        document.body.style.backgroundImage = `url("${gifs[mobileIndex]}")`;
+        mobileIndex = (mobileIndex + 1) % gifs.length;
     }
 }
